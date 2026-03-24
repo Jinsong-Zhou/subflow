ed315a0eee586caddb6378ff409384723c7e369b
Jinsong-Zhou <757551765@qq.com>
2026-03-24 13:22:00 +0800

    chore: scaffold Xcode project with xcodegen and WhisperKit dependency

---
+++ TranslatedCaption/TranslatedCaptionApp.swift
@@ -0,0 +1,11 @@
+import SwiftUI
+
+@main
+struct TranslatedCaptionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            Text("TranslatedCaption")
+                .frame(width: 600, height: 400)
+        }
+    }
+}
